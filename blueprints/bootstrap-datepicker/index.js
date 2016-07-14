6b5f6e9f666460aa5c232295a36426646efcf1dd
Louie Penaflor <lpenaflor@abodo.com>
2016-07-14 14:49:40 -0500

    Updated blueprint

--- blueprints/bootstrap-datepicker/index.js
+++ blueprints/bootstrap-datepicker/index.js
@@ -2,6 +2,7 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('bootstrap-datepicker');
+    //return this.addBowerPackageToProject('bootstrap-datepicker');
+    return this.addBowerPackageToProject('https://github.com/abodo-dev/bootstrap-datepicker.git#dropdown-menu-style');
   }
 };
